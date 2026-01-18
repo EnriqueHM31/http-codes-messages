55c51d5cbc8d0ccf105619707e72263c1e574cc2
Luis Hernandez <luisenriquehernandezmarin0@gmail.com>
2026-01-18 13:20:05 -0600

    Fix character image extraction in getImageResource function by ensuring alt text is correctly assigned, enhancing image handling consistency.

--- src/adapters/ImageResoruce.ts
+++ src/adapters/ImageResoruce.ts
@@ -3,8 +3,8 @@ import type { HttpImage } from '../types';
 export function getImageResource(images: HttpImage[]) {
 
     const coverImage = images?.[0]?.imageCover;
-    const characterImage = images?.[1]?.imageCharacter;
     const coverImageAlt = images?.[0]?.alt;
+    const characterImage = images?.[1]?.imageCharacter;
     const characterImageAlt = images?.[1]?.alt;
 
     return {
