d8ecf6e19c80467c2847e5ea200bc4aed6444514
Luis Hernandez <luisenriquehernandezmarin0@gmail.com>
2026-01-18 13:33:32 -0600

    Refactor components to utilize type definitions from components.d.ts, enhancing type safety and clarity across CodeBlocks, SectionCard, InfoCard, and various detail code sections. Update prop handling in multiple files for improved maintainability.

--- src/types/components.d.ts
+++ src/types/components.d.ts
@@ -1,3 +1,9 @@
+import type { HttpCode, HttpExample } from ".";
+
+export interface LayoutProps {
+    title: string;
+    descripcion: string;
+}
 export interface CardAnimadaProps {
     description: string;
     imageCover: string;
@@ -7,4 +13,66 @@ export interface CardAnimadaProps {
     image_logo: string;
     link: string;
     code: string;
+}
+
+export interface CodeBlockProps {
+    title: string;
+    content: string;
+}
+
+export interface SectionCardProps {
+    title: string;
+}
+
+export interface TitleSectionProps {
+    text: string;
+    align?: 'left' | 'center' | 'right';
+    color?: string;
+    size?: string;
+    delay?: string;
+}
+
+export interface InfoCardProps {
+    title: string;
+    value: string;
+}
+
+
+export interface PropsMetaDataCode {
+    category: string;
+    httpVersion: string;
+    specification: string;
+    relatedCodes: string[];
+}
+
+export interface SectionCardsProps {
+    data: HttpCode[];
+    URL_LOGO: string;
+    PAGE_SIZE: number;
+}
+
+export interface PropsUseCasesCode {
+    useCases: string[];
+}
+
+export interface PropsDescriptionCode {
+    description: string;
+}
+
+export interface PropsExamplesCode {
+    examples: HttpExample[];
+}
+
+export interface HeaderDetailsProps {
+    cover: string;
+    alt: string;
+    code: string;
+}
+
+export interface PropsTechnicalNotesCode {
+    technicalNotes: string;
+}
+
+export interface CodesHttpProps {
+    data: HttpCode[];
 }
