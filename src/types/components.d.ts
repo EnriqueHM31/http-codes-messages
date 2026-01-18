051d72e6ef11722c5e8285c8ff94b1162801a798
Luis Hernandez <luisenriquehernandezmarin0@gmail.com>
2026-01-18 13:20:01 -0600

    Add CardAnimadaProps interface to components.d.ts for defining properties of animated cards, enhancing type safety and clarity in component usage.

---
+++ src/types/components.d.ts
@@ -0,0 +1,10 @@
+export interface CardAnimadaProps {
+    description: string;
+    imageCover: string;
+    imageCoverAlt: string;
+    imageCharacter: string;
+    imageCharacterAlt: string;
+    image_logo: string;
+    link: string;
+    code: string;
+}
